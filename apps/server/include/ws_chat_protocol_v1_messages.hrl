ee8c239e603d0f6a6ac3980344e32f867a253d25
Vorontsov Nikita <NikitaV@meforo.com>
2017-04-19 10:57:10 +0700

    chat protocol test suite scratch

--- apps/server/include/ws_chat_protocol_v1_messages.hrl
+++ apps/server/include/ws_chat_protocol_v1_messages.hrl
@@ -4,32 +4,33 @@
 %% Client-to-Server
 -record(c2s_chat_get_list, {}).
 -record(c2s_chat_get_info, {chat_id  :: integer()}).
--record(c2s_chat_create, {name :: binary(), user_id :: [integer()]}).
--record(c2s_chat_leave, {}).
--record(c2s_chat_delete, {}).
--record(c2s_chat_invite_user, {}).
--record(c2s_chat_mute, {}).
--record(c2s_chat_unmute, {}).
--record(c2s_chat_typing, {}).
--record(c2s_message_send, {}).
--record(c2s_message_get_list, {}).
--record(c2s_message_update, {}).
--record(c2s_message_update_status, {}).
+-record(c2s_chat_create, {name :: binary(), user_msisdn :: [integer()]}).
+-record(c2s_chat_leave, {chat_id :: integer()}).
+-record(c2s_chat_delete, {chat_id :: integer()}).
+-record(c2s_chat_invite_user, {chat_id :: integer(), user_msisdn :: integer() }).
+-record(c2s_chat_mute, {chat_id :: integer()}).
+-record(c2s_chat_unmute, {chat_id :: integer()}).
+-record(c2s_chat_typing, {chat_id :: integer()}).
+-record(c2s_message_send, {chat_id :: integer(), msg_body :: binary()}).
+-record(c2s_message_get_list, {}).              %TODO
+-record(c2s_message_update, {msg_id :: integer(), msg_body :: binary()}).
+-record(c2s_message_update_status, {msg_id :: integer()}).
 -record(c2s_system_logout, {}).
--record(c2s_user_get_info, {}).
--record(c2s_user_get_status, {}).
--record(c2s_user_set_info, {}).
--record(c2s_user_search, {}).
+-record(c2s_user_get_info, {user_msisdn :: integer()}).
+-record(c2s_user_get_status, {user_msisdn :: integer()}).
+-record(c2s_user_set_info, {user_msisdn :: integer(), fname :: binary(), lname :: binary(), age :: non_neg_integer(), is_male :: boolean()}).
+-record(c2s_user_search, {}).                   %TODO: нужно ли? чем отличается от get_info?
 -record(c2s_room_get_tree, {}).
--record(c2s_room_get_info, {}).
--record(c2s_room_rename, {}).
--record(c2s_room_add_user, {}).
--record(c2s_room_del_user, {}).
--record(c2s_room_add_subroom, {}).
--record(c2s_room_create, {}).
--record(c2s_room_delete, {}).
--record(c2s_room_enter_to_chat, {}).
--record(c2s_room_send_message, {}).
+-record(c2s_room_get_info, {room_id :: integer(), subroom_id :: list(), user_msisdn :: integer(), chat_id :: integer(), room_name :: binary()}).
+-record(c2s_room_rename, {room_id :: integer(), room_name :: binary()}).
+-record(c2s_room_add_user, {room_id :: integer(), user_msisdn :: integer()}).
+-record(c2s_room_del_user, {room_id :: integer(), user_msisdn :: integer()}).
+-record(c2s_room_add_subroom, {room_id :: integer(), subroom_id :: integer()}).
+-record(c2s_room_create, {room_name :: binary()}).
+-record(c2s_room_delete, {room_id :: integer()}).
+-record(c2s_room_enter_to_chat, {room_id :: integer(), chat_id :: integer()}).
+-record(c2s_room_send_message, {room_id :: integer(), msg_body :: binary(), is_recursive :: boolean()}).
+%% TODO: room_chat operations
 
 -type client_msg_type() ::   #c2s_chat_get_list{}
                            | #c2s_chat_get_info{}
@@ -59,26 +60,26 @@
 
 %% Server-to-Client
 -record(s2c_chat_list, {chat_id = [] :: [integer()]}).
--record(s2c_chat_info, {chat_id, name, user_id = [], is_muted, is_writable, chat_owner}).
+-record(s2c_chat_info, {chat_id, name, user_msisdn = [], is_muted, is_writable, chat_owner}).
 -record(s2c_chat_create_result, {result_code, chat_id}).
 -record(s2c_chat_leave_result, {result_code, chat_id}).
 -record(s2c_chat_delete_result, {result_code, chat_id}).
--record(s2c_chat_invite_user_result, {result_code, chat_id, user_id}).
+-record(s2c_chat_invite_user_result, {result_code, chat_id, user_msisdn}).
 -record(s2c_chat_mute_result, {result_code, chat_id}).
 -record(s2c_chat_unmute_result, {result_code, chat_id}).
--record(s2c_chat_typing, {chat_id, user_id}).
+-record(s2c_chat_typing, {chat_id, user_msisdn}).
 -record(s2c_message, {chat_id, msg_body, timestamp, status, msg_id}).
 -record(s2c_message_update, {chat_id, msg_id, msg_body}).
 -record(s2c_message_update_status, {chat_id, msg_id}).
--record(s2c_user_info, {user_id, fname, lname, age, sex}).
--record(s2c_user_status, {user_id, is_online = 'false', last_visit_timestamp}).
+-record(s2c_user_info, {user_msisdn, fname, lname, age, sex}).
+-record(s2c_user_status, {user_msisdn, is_online = 'false', last_visit_timestamp}).
 -record(s2c_user_set_info_result, {result_code, fname, lname, age, sex}).
--record(s2c_user_search_result, {user_id = []}).
+-record(s2c_user_search_result, {user_msisdn = []}).
 -record(s2c_room_list, {room_id = []}).
--record(s2c_room_info, {room_id, subroom_id = [], user_id = [], chat_id = []}).
+-record(s2c_room_info, {room_id, subroom_id = [], user_msisdn = [], chat_id = []}).
 -record(s2c_room_rename_result, {result_code, room_id, name}).
--record(s2c_room_add_user_result, {result_code, room_id, user_id, access_level, is_public = 'false'}).
--record(s2c_room_del_user_result, {result_code, room_id, user_id}).
+-record(s2c_room_add_user_result, {result_code, room_id, user_msisdn, access_level, is_public = 'false'}).
+-record(s2c_room_del_user_result, {result_code, room_id, user_msisdn}).
 -record(s2c_room_add_subroom_result, {result_code, room_id, subroom_id}).
 -record(s2c_room_create_result, {result_code, room_id, name}).
 
---
+++ apps/server/test/chat_protocol_SUITE.erl
@@ -0,0 +1,165 @@
+%%%-------------------------------------------------------------------
+%%% @author ne <ne@ne>
+%%% @copyright (C) 2017, ne
+%%% @doc
+%%%
+%%% @end
+%%% Created : 19 Apr 2017 by ne <ne@ne>
+%%%-------------------------------------------------------------------
+-module(chat_protocol_SUITE).
+
+-compile(export_all).
+
+-include_lib("common_test/include/ct.hrl").
+-include("ws_chat_protocol_v1_messages.hrl").
+-include("server.hrl").
+
+%%--------------------------------------------------------------------
+%% @spec suite() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+suite() ->
+    [{timetrap,{seconds,30}}].
+
+%%--------------------------------------------------------------------
+%% @spec init_per_suite(Config0) ->
+%%     Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    User = users:new(89628323872, <<"121">>, <<"Nikita">>, <<"Vorontsov">>, 25, 'true', 'administrators', 0),
+    [{user, User} | Config].
+
+%%--------------------------------------------------------------------
+%% @spec end_per_suite(Config0) -> term() | {save_config,Config1}
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(Config) ->
+    User = proplists:get_value(user, Config),
+    users:delete(User),
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_group(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_group(GroupName, Config0) ->
+%%               term() | {save_config,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_testcase(TestCase, Config0) ->
+%%               term() | {save_config,Config1} | {fail,Reason}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec groups() -> [Group]
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%% Shuffle = shuffle | {shuffle,{integer(),integer(),integer()}}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%% N = integer() | forever
+%% @end
+%%--------------------------------------------------------------------
+groups() ->
+    [{chats, [], [chat_get_list_test
+                 ,chat_get_info_test
+                 ,chat_create_test
+                 ,chat_leave_test
+                 ,chat_delete_test
+                 ,chat_invite_user_test
+                 ,chat_mute_test
+                 ,chat_unmute_test
+                 ,chat_typing_test]}
+    ,{messages, [], [message_send_test
+                    ,message_get_list_test
+                    ,message_update_test
+                    ,message_update_status_test]}
+    ,{users, [], [user_get_info_test
+                 ,user_get_status_test
+                 ,user_set_info_test
+                 ,user_search_test]}
+    ,{rooms, [], [room_get_tree_test
+                 ,room_get_info_test
+                 ,room_rename_test
+                 ,room_add_user_test
+                 ,room_del_user_test
+                 ,room_add_subroom_test
+                 ,room_create_test
+                 ,room_delete_test
+                 ,room_enter_to_chat_test
+                 ,room_send_message_test]}
+    ,{system, [], [system_logout_test]}].
+
+%%--------------------------------------------------------------------
+%% @spec all() -> GroupsAndTestCases | {skip,Reason}
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%% TestCase = atom()
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+all() ->
+    [{group, chats}
+    ,{group, messages}
+    ,{group, users}
+    ,{group, rooms}
+    ,{group, system}].
+
+%%--------------------------------------------------------------------
+%% @spec TestCase() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+my_test_case() ->
+    [].
+
+%%--------------------------------------------------------------------
+%% @spec TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% Comment = term()
+%% @end
+%%--------------------------------------------------------------------
+my_test_case(_Config) ->
+    ok.
