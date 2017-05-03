3ebfe4336a8e54683350351ff1b45db510c4487e
Vorontsov Nikita <noobsenslaver@mail.ru>
2017-05-03 17:36:00 +0700

    rest_sms_protocol_v1

---
+++ apps/server/src/rest/rest_sms_protocol_v1.erl
@@ -0,0 +1,86 @@
+%%%-------------------------------------------------------------------
+%%% @author Vorontsov Nikita <noobsenslaver@mail.ru>
+%%% @copyright (C) 2017,
+%%% @doc
+%%%
+%%% @end
+%%% Created :  2 May 2017
+%%%-------------------------------------------------------------------
+-module(rest_sms_protocol_v1).
+-behaviour(rest_protocol_behaviour).
+-include("server.hrl").
+
+-define(SMS_SERVER_URL, "https://sms.ru/sms/send").
+-ifdef(TEST).
+-define(BODY(AppId, MSISDN, Code), "api_id=" ++ AppId ++ "&to=+" ++ erlang:integer_to_list(MSISDN) ++ "&text=" ++ erlang:integer_to_list(Code) ++ "&test=1").
+-define(GET_APP_ID, {ok, "25B1DB2A-4F68-27D4-3FD6-4DC122309D6B"}).
+-define(SMS_RESEND_INTERVAL, {ok, 60}).
+-else.
+-define(GET_APP_ID, application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'sms_api_id')).
+-define(BODY(AppId, MSISDN, Code), "api_id=" ++ AppId ++ "&to=+" ++ erlang:integer_to_list(MSISDN) ++ "&text=" ++ erlang:integer_to_list(Code)).
+-define(SMS_RESEND_INTERVAL, application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'sms_resend_interval')).
+-endif.
+
+-export([handle/4
+        ,access_level/1
+        ,allowed_groups/1]).
+
+%% Required fields:
+%% msisdn
+-spec handle(method(), cowboy_req:req(), #q_state{}, [binary()]) -> {cowboy_req:req(), #q_state{}, [binary()]}.
+handle(<<"POST">>, Req, State, _Args) ->
+    lager:md([{'appname', <<"server->sms">>}]),
+    {IP, _Port} = cowboy_req:peer(Req),
+    {ok, ResendInterval} = ?SMS_RESEND_INTERVAL,
+    {ok, AppId} = ?GET_APP_ID,
+    {MSec, Sec, _} = erlang:timestamp(),
+    Now = MSec * 1000000 + Sec,
+    NewState = case common:get_body_data(Req) of
+                   #{<<"msisdn">> := BMSISDN} ->
+                       MSISDN = common:to_integer(BMSISDN),
+                       lager:debug("IP: ~p require to send sms to MSISDN: ~p~n", [IP, MSISDN]),
+                       case sms:new(MSISDN) of
+                           {exists, TimeStamp, _Code} when Now - TimeStamp < ResendInterval ->  %too fast
+                               State#q_state{code = 429};
+                           {exists, _TimeStamp, Code} ->                                        %resend
+                               sms:update_timestamp(MSISDN),
+                               Resp = send_sms(AppId, MSISDN, Code),
+                               State#q_state{code = Resp};
+                           'false' ->
+                               lager:error("Error on sms:new()", []),
+                               State#q_state{code = 500};
+                           Code ->
+                               Resp = send_sms(AppId, MSISDN, Code),
+                               State#q_state{code = Resp}
+                           end;
+                   _ ->
+                       State#q_state{code = 400} % bad request
+               end,
+    {Req, NewState, _Args};
+handle(_Method, Req, State, _Other)->
+    {Req, State#q_state{code = 405}, []}.
+
+access_level(_Method) ->
+    'infinity'.
+
+allowed_groups(_Method) ->
+    ['administrators', 'users', 'guests'].
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+send_sms(AppId, MSISDN, Code) ->
+    case httpc:request(post, {?SMS_SERVER_URL, [], "application/x-www-form-urlencoded", ?BODY(AppId, MSISDN, Code)}, [], []) of
+        {ok, {{_, 200, _}, _, RespBody}} ->
+            case erlang:list_to_integer(lists:takewhile(fun(X)-> X /= $\n end, RespBody)) of
+                100 ->
+                    lager:debug("sms sended successfuly to MSISDN: ~p~n", [MSISDN]),
+                    200;
+                _Other ->
+                    lager:error("Error on send sms to MSISDN ~p, error: ~p~n", [MSISDN, _Other]),
+                    500
+            end;
+        _Error ->
+            lager:error("Error on send sms to MSISDN ~p, error: ~p~n", [MSISDN, _Error]),
+            500
+    end.
