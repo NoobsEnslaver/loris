17104bd37ab236f240285e9b7abd303b6d195a8a
Vorontsov Nikita <noobsenslaver@mail.ru>
2017-05-17 16:57:08 +0700

    go to tls

--- apps/server/src/server_app.erl
+++ apps/server/src/server_app.erl
@@ -23,9 +23,15 @@
 -spec start('normal' | {'failover',atom()} | {'takeover',atom()}, any()) -> {'ok', pid()} | {'error', any()}.
 start(_Type, _Args) ->
     lager:md([{'appname', ?APP_NAME}]),
-    TcpOpts = application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'tcp_params', [{'port', 8080}
-                                                           ,{'buffer', 32768}
-                                                           ,{'max_connections', 65536}]),
+    PrivDir = code:priv_dir(server),
+    {ok, CertFile} = application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'certfile'),
+    {ok, KeyFile} = application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'keyfile'),
+    TcpOpts1 = application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'tcp_params', [{'port', 8080}
+                                                                                   ,{'buffer', 32768}
+                                                                                   ,{'max_connections', 65536}]),
+    TcpOpts = [{certfile, PrivDir ++ CertFile}|
+               [{keyfile, PrivDir ++ KeyFile} | TcpOpts1]],
+    io:format("TcpOptions: ~p~n", [TcpOpts]),
     Acceptors = application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'acceptors', 100),
     StaticDir = application:get_env(binary_to_atom(?APP_NAME, 'utf8'), 'static_dir', "/srv"),
     Dispatch = cowboy_router:compile(
@@ -39,7 +45,8 @@ start(_Type, _Args) ->
                     ,{'_', 'server_404_handler', []}]
                   }]),
     ProtocolOpts = #{env => #{dispatch => Dispatch}},
-    {'ok', _Pid} = cowboy:start_clear(?LISTENER_NAME, Acceptors, TcpOpts, ProtocolOpts),
+    {'ok', _Pid} = cowboy:start_tls(?LISTENER_NAME, Acceptors, TcpOpts, ProtocolOpts),
+
     server_sup:start_link().               %dummy
 
 %%--------------------------------------------------------------------
