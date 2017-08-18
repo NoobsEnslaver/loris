64e3e8077eb33e295575b3e4a95e6ccbd872f444
Nikita Vorontsov <noobsenslaver@mail.ru>
2017-08-18 20:10:20 +0700

    let's encrypt cert's updated, challenge check mechanism

---
+++ apps/server/src/server_acme_handler.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author Vorontsov Nikita <noobsenslaver@mail.ru>
+%%% @copyright (C) 2017
+%%% @doc
+%%%
+%%% @end
+%%% Created : 18 Aug 2017
+%%%-------------------------------------------------------------------
+-module(server_acme_handler).
+-behaviour(cowboy_handler).
+
+-export([init/2
+        ,handle/2
+        ,terminate/3]).
+-include("server.hrl").
+
+-spec init(cowboy_req:req(), map()) -> {'ok', cowboy_req:req(), []}.
+init(Req, _Opts) ->
+    Key = cowboy_req:binding('acme_key', Req, <<"">>),
+    KeyVal = application:get_env('server', 'acme_challenge', []),
+    Resp = case proplists:get_value(Key, KeyVal) of
+               'undefined' ->
+                   cowboy_req:reply(404, #{<<"content-type">> => <<"text/html">>}, <<"">>, Req);
+               Val ->
+                   cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Val, Req)
+           end,
+    {'ok', Resp, []}.
+
+handle(_Req, _State) ->
+    {'shutdown', _Req, _State}.
+
+terminate(_Reason, _Req, _State) ->
+    'ok'.
